ffad1deb5ae99ad9c27c8e24295287e2714e5b0d
neetu <neetuagw@gmail.com>
2016-08-25 11:17:11 +0100

    Update description parameter

--- plugin/src/ios/OnyxbeaconPhonegap.m
+++ plugin/src/ios/OnyxbeaconPhonegap.m
@@ -99,6 +99,7 @@
                 coupon.title ? [NSString stringWithString:coupon.title] : @"", @"title",
                 coupon.path ? [NSString stringWithString:coupon.path] : @"", @"path",
                 coupon.message ? [NSString stringWithString:coupon.message] : @"", @"message",
+                coupon.description ? [NSString stringWithString:coupon.description] : @"", @"description",
                 coupon.uuid ? [NSNumber numberWithInt:coupon.uuid] : @"", @"uuid",
                 nil
             ];
